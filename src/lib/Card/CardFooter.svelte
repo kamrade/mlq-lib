b77b9e02f4f58e066e70b3e19f473f9b2a72498b
Dennis Michailov <denis.michailov@gmail.com>
2025-08-28 20:56:19 +0300

    Card

---
+++ src/lib/Card/CardFooter.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+
+  let { children }: { children: Snippet } = $props();
+</script>
+
+<div class="CardFooter">
+  {@render children()}
+</div>
+
+<style lang="scss">
+
+  .CardFooter {
+    padding: 0 1.5rem;
+    display: flex;
+    flex-direction: column;
+    gap: .5rem;
+  }
+
+</style>
--- src/routes/showcase/card/+page.svelte
+++ src/routes/showcase/card/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-  import { Card, CardHeader, CardTitle, CardDescription, CardContent, TextInput } from '@lib';
+  import { Card, CardFooter, CardHeader, CardTitle, CardDescription, CardContent, TextInput, ButtonMilk } from '@lib';
   import Label from "@lib/Label/Label.svelte";
+  import { CheckboxCircleFillSystem } from "svelte-remix";
 </script>
 
 <h2 class="text-2xl font-medium mb-4">Card</h2>
@@ -8,26 +9,38 @@
 <hr class="mt-4 mb-8 border-t border-gray-300" />
 
 <div class="mb-3">
-  <Card>
-    <CardHeader>
-      <CardTitle>
-        Card title
-      </CardTitle>
-      <CardDescription>
-        Card description
-      </CardDescription>
-    </CardHeader>
-    <CardContent>
-      <Label forValue="input-inside-of-the-label">
-        <p class="mb-1">Test label</p>
-        <TextInput id="input-inside-of-the-label" placeholder="Test Placeholder" />
-      </Label>
-      <Label forValue="input-2">
-        <p class="mb-1">Test label</p>
-        <TextInput id="input-2" placeholder="Test Placeholder" />
-      </Label>
-    </CardContent>
-  </Card>
+  <div class="grid grid-cols-8 gap-3">
+    <div class="col-span-4">
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            Card title
+          </CardTitle>
+          <CardDescription>
+            Card description
+          </CardDescription>
+        </CardHeader>
+        
+        <CardContent>
+          <Label forValue="input-inside-of-the-label">
+            <p class="mb-1">Test label</p>
+            <TextInput id="input-inside-of-the-label" placeholder="Test Placeholder" />
+          </Label>
+          <Label forValue="input-2">
+            <p class="mb-1">Test label</p>
+            <TextInput id="input-2" placeholder="Test Placeholder" />
+          </Label>
+        </CardContent>
+
+        <CardFooter>
+          <ButtonMilk variant="base-emphasis" size="lg"><CheckboxCircleFillSystem size="1em"/>Confirm</ButtonMilk>
+          <ButtonMilk variant="base-contained" size="lg"><CheckboxCircleFillSystem size="1em"/>Cancel</ButtonMilk>
+        </CardFooter>
+
+      </Card>
+    </div>
+
+  </div>
 </div>
 
 <style lang="scss">
